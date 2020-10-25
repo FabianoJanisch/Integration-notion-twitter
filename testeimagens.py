d9e8613b3a7ed06fd3b23d479cfba6bc5df4afe8
Janisckinho5 <fabianoaugustojanisch@hotmail.com>
2020-10-25 11:21:58 -0300

    bottwittertimeline

--- testeimagens.py
+++ testeimagens.py
@@ -16,23 +16,27 @@ while True:
     time.sleep(25)
     hora = time.gmtime()
     if hora[3] == 10 and hora[4] == 00:
-        listatimeline = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.jpeg', '8.png', '9.png', '10.png', '11.jpeg', 
+        listatimeline = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.jpg', '7.jpeg', '8.png', '9.png', '10.png', '11.jpeg', 
         '12.png', '13.png', '14.png', '15.png', '16.png', '17.png']
         fotoline = random.choice(listatimeline)
         imageline = Image.open(fotoline)
 
         listatexto = ['Essa foi boa amigo', 'Vou mimir', 'Hey hey gatinha', 'Oi casada', 'Minha regra é clara, se\nenvolve transar não vale\na pena', 
-        'Bom dia cambada dia de\ncheirar pó', 'Hey hey gatinha passa o zap', 'Eu tenho muita sorte de ser\nperfeito, ta ligado?']
+        'Bom dia cambada dia de\ncheirar pó', 'Hey hey gatinha passa o zap', 'Eu tenho muita sorte de ser\nperfeito, ta ligado?', 
+        'Um gostosão como eu ninguém\nresiste não é mesmo?']
         
         textorandom = random.choice(listatexto)
         textorandom2 = textorandom.split()
         textorandom3 = ' '.join(textorandom2)
 
         fontline = ImageFont.truetype("arial.ttf", 80)
+        if textorandom == listatexto[-1]:
+            fontline = ImageFont.truetype("arial.ttf", 75)
+        color = (255, 255, 255)
 
         imageline = imageline.resize((1080, 1080))
         drawline = ImageDraw.Draw(imageline)
-        drawline.multiline_text((000, 000), (textorandom),fill=(255, 255, 255), font=fontline)
+        drawline.multiline_text((000, 000), (textorandom),fill=(color), font=fontline)
 
 
         imageline.save('amigotimeline.png')
