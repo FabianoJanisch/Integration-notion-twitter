b0d73759a238507139aa60d20aee37e5fd033fba
Fabiano Janisch <fabianoaugustojanisch@hotmail.com>
2021-07-07 20:48:07 -0300

    bottwittertimeline

--- testeimagens.py
+++ testeimagens.py
@@ -16,14 +16,14 @@ while True:
     time.sleep(25)
     hora = time.gmtime()
     if hora[3] == 10 and hora[4] == 00:
-        listatimeline = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.jpg', '7.png', '8.png', '9.png', '10.png', 
+        listatimeline = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.jpg', '7.png', '8.png', '9.png', '10.png', '11.png', 
         '12.png', '13.png', '14.png', '15.png', '16.png', '17.png', '18.png']
         fotoline = random.choice(listatimeline)
         imageline = Image.open(fotoline)
 
-        listatexto = ['Vou te levar para ver os incríveis...\nos incríveis beijos que quero te dar', 'Se eu fosse um super herói\nmeu poder ia ser querer ter vc', 'Para o relacionamento dar certo\n os dois tem que ser eu e vc bb', 
-        '       Eu até casaria comigo\n     mas essa honra eu deixo\n                   pra vc', 'Sim eu sou gótico...\n\n\nGamado em você\n\nOuvinte da tua linda voz\n\nTe amo\n\nIndignado por tanta beleza\n\nCom seu sorriso eu desmaio\n\nOu vamo namora', 
-        'Eu fui no médico e ele falou que\n       preciso de uma vitamina\n               chamada vc', 'Você não é raiz quadrada de 16\n     mas queria sempre poder\n          te deixar de quatro']
+        listatexto = ['      Usei shampoo anti quedas\n  e ainda continuo caindo por você', '  Queria te desejar boa noite\n mas só consigo desejar você', '    Você aceita casar comigo\n       e ter vários gatinhos?', 
+        'Topa jogar UNO comigo?\nUNO em cima do outro', '      Se tudo na vida passa, por que\nvocê não passa sua vida ao meu lado?', 
+        'Tá frio né? Se quiser um casaco\n           é só casarcomigo', '               O que é nude??\n Poderia me mandar um exemplo?']
         if hora[6] == 4:
             listatexto = ["Bom dia hoje tem sexta dos\ncria", " Sextou, mas qual a graça de\n         sextar sem você??"]
         
@@ -34,10 +34,12 @@ while True:
 
         color = (255, 255, 255)
         fontline = ImageFont.truetype("arial.ttf", 80)
-        if "os incríveis" in textorandom or "Quando sua mãe fala" in textorandom:
+        if "continuo caindo por" in textorandom or "que é nude?" in textorandom:
             fontline = ImageFont.truetype("arial.ttf", 69)
-        if 'relacionamento dar' in textorandom or "fui no médico" in textorandom or "deixar de quatro" in textorandom:
+        if 'relacionamento dar' in textorandom or "casaco" in textorandom or "deixar de quatro" in textorandom:
             fontline = ImageFont.truetype("arial.ttf", 75)
+        if 'tudo na vida' in textorandom:
+            fontline = ImageFont.truetype("arial.ttf", 62)
 
 
         imageline = imageline.resize((1080, 1080))
