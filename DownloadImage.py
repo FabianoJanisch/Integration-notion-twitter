29f93dfc045d817f1220bd4f542a0f3c73355d24
Fabiano Janisch <fabianoaugustojanisch@hotmail.com>
2021-12-17 17:12:46 -0300

    make it better

---
+++ DownloadImage.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from random import randint
+from bs4 import BeautifulSoup
+
+
+
+def readDatabase():
+    NOTION_KEY = "secret_iZowESNGYt5uQqaCFNjwk7WrD3gNoGKaGs1OYsVPhaB"
+    databaseId = '647a9057c41049519cc14914f44e8495'
+    headers =  {
+        "Authorization": "Bearer " + NOTION_KEY,
+        "Content-Type": "application/json",
+        "Notion-Version": "2021-08-16"
+    }
+
+
+    #Request site
+    readUrl = f'https://api.notion.com/v1/databases/{databaseId}/query'
+    res = requests.request("POST", readUrl, headers=headers)
+    data2 = BeautifulSoup(res.content, "html.parser").text
+    soup = json.loads(data2)
+
+
+    #Random number
+    numbersimage =  len(soup['results'])
+    randomizeimage = randint(0, numbersimage-1)
+
+    #Localize image
+    urlPage = soup['results'][randomizeimage]['url'].split('-')[-1]
+    urlPageC = f'https://api.notion.com/v1/blocks/{urlPage}/children'
+    resquestImage = requests.get(urlPageC, headers=headers)
+    ImageD = BeautifulSoup(resquestImage.content, "html.parser").text
+    soupImageD = json.loads(ImageD)
+
+    #Download image
+    ImageDownload = soupImageD['results'][0]['image']['file']['url']
+    ImageOpen = open('IMGDownload.png', 'wb')
+    responseIMG = requests.get(ImageDownload)
+    ImageOpen.write(responseIMG.content)
+    ImageOpen.close()
+
+
--- testeimagens.py
+++ testeimagens.py
@@ -2,6 +2,7 @@ import tweepy
 import time
 import random
 from PIL import Image, ImageDraw, ImageFont
+from DownloadImage import readDatabase
 
 API_KEY = '8TtCsXN7WOzWX9hEMQWIoXAZq'
 API_SKEY = 'h0Azcyi6Tpv7Nqi9WofEcVZnuLgvLbIXWsMcMrvZ7tnYyUA5Gf'
@@ -10,17 +11,14 @@ ACESS_SKEY = 'lweAKtZEitTIlBXnLy3rndNJYfq0RKTIfwXvZmsILj1SK'
 
 auth = tweepy.OAuthHandler(API_KEY, API_SKEY)
 auth.set_access_token(ACESS_KEY, ACESS_SKEY)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tweepy.API(auth)
 
 while True:
     time.sleep(25)
     hora = time.gmtime()
     if hora[3] == 10 and hora[4] == 00:
-        listatimeline = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png', 
-        '12.png', '13.png', '14.png', '15.png', '16.png', '17.png']
-        fotoline = random.choice(listatimeline)
-        imageline = Image.open(fotoline)
-
+        ImageO = readDatabase()
+        imageline = Image.open('IMGDownload.png')
         listatexto = ['QUEM É ESSE GOSTOSO??\n                ata sou eu', 'tt = twitter\nttk = tiktok\nig = instagram\nvc = quer namorar comigo?', '    Como que eu vou mandar bom dia\npra ela, se ela só acorda depois das 12h', 
         'Do que adianta ter duas mãos,se vc não\n   vai usar nenhuma pra me dar carinho', ' Dorme bem, pq francamente...\n          boa noite só comigo', 
         'Quero nada sério com você não,\n  só casar e morar junto ta bom', '         E fora da internet?\n      vamos ficar quando??']
@@ -49,6 +47,6 @@ while True:
 
         imageline.save('amigotimeline.png')
 
-        api.update_with_media ('amigotimeline.png', (textorandom3))
+        api.update_status_with_media (textorandom3, 'amigotimeline.png')
         print ("(Tweet enviado na timeline)")
         time.sleep(82.800)
